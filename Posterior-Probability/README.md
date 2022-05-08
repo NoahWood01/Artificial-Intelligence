26eae70e5a59b69e6de1f435ba2df2543ea335a0
NoahWood01 <noahdanielwood@gmail.com>
2022-05-07 21:49:39 -0500

    Correct Observation Computation

---
+++ Posterior-Probability/README.md
@@ -0,0 +1,18 @@
+# Posterior Probability #
+
+Determine the posterior probability of different hypotheses, given priors for these hypotheses, and given a sequence of observations, and determine the probability that the next observation will be of a specific type, priors for different hypotheses, and given a sequence of observations.
+
+The C's and L's represent the observations, there can be any number of either.
+
+### Command Line Arguments ###
+
+Put in this form:
+
+`compute_a_posteriori observations`
+
+### Execute with ###
+
+Compile with python using `python` `py` or `python3` based on by your settings.
+
+
+`python compute_a_posteriori CLLCCLLLCCL`
--- Posterior-Probability/compute_a_posteriori.py
+++ Posterior-Probability/compute_a_posteriori.py
@@ -3,12 +3,12 @@
 
 import sys
 
-# get command line arguments
+# get number command line arguments
 numArgs = len(sys.argv)
 
 # check args
 if numArgs > 2:
-    print("incorrect arguments, need: python3 compute_a_posteriori.py <observations>")
+    print("incorrect arguments, format: python compute_a_posteriori.py <observations>")
     exit(0)
 
 # inital probabilities for each bag
@@ -41,18 +41,56 @@ try:
     file = open("result.txt", "w")
     if len(sys.argv) > 1:
         file.write("Observation Sequence Q: " + sys.argv[1])
-        file.write("Length of Q: \n")
+        file.write("\nLength of Q: " + str(len(sys.argv[1])))
+        counter = 1
+
+        for char in sys.argv[1]:
+            # evaluate based on the observation
+            # other characters than 'C' or 'L' will be ignored.
+            if char == 'C':
+                h1 = pc1 * h1 / pNextC
+                h2 = pc2 * h2 / pNextC
+                h3 = pc3 * h3 / pNextC
+                h4 = pc4 * h4 / pNextC
+                h5 = pc5 * h5 / pNextC
+
+                # get the probaility that next is cherry and for lime
+                pNextC = h1 * pc1 + h2 * pc2 + h3 * pc3 + h4 * pc4 + h5 * pc5
+                pNextL = h1 * pl1 + h2 * pl2 + h3 * pl3 + h4 * pl4 + h5 * pl5
+                
+            elif char == 'L':
+                h1 = pl1 * h1 / pNextL
+                h2 = pl2 * h2 / pNextL
+                h3 = pl3 * h3 / pNextL
+                h4 = pl4 * h4 / pNextL
+                h5 = pl5 * h5 / pNextL
+
+                # get the probaility that next is cherry and for lime
+                pNextC = h1 * pc1 + h2 * pc2 + h3 * pc3 + h4 * pc4 + h5 * pc5
+                pNextL = h1 * pl1 + h2 * pl2 + h3 * pl3 + h4 * pl4 + h5 * pl5
+
+            # display
+            file.write("\n\nAfter Observation " + str(counter) + " = " + char + "\n")
+            counter += 1
+            file.write("P(h1 | Q) = " + str(round(h1,5)) + "\n")
+            file.write("P(h2 | Q) = " + str(round(h2,5)) + "\n")
+            file.write("P(h3 | Q) = " + str(round(h3,5)) + "\n")
+            file.write("P(h4 | Q) = " + str(round(h4,5)) + "\n")
+            file.write("P(h5 | Q) = " + str(round(h5,5)) + "\n")
+            file.write("\nProbaility that the next candy we pick will be C, given Q: " + str(round(pNextC,5)))
+            file.write("\nProbaility that the next candy we pick will be L, given Q: " + str(round(pNextL,5)))
     else:
+        # since no observations, display current probabilities
+
         file.write("Observation Sequence Q: none\n")
         file.write("Length of Q: 0\n\n")
-        file.write("P(h1 | Q) = " + str(h1) + "\n")
-        file.write("P(h2 | Q) = " + str(h2) + "\n")
-        file.write("P(h3 | Q) = " + str(h3) + "\n")
-        file.write("P(h4 | Q) = " + str(h4) + "\n")
-        file.write("P(h5 | Q) = " + str(h5) + "\n")
-        file.write("\nProbaility that the next candy we pick will be C, given Q: " + str(pNextC))
-        file.write("\nProbaility that the next candy we pick will be L, given Q: " + str(pNextL))
-
+        file.write("P(h1 | Q) = " + str(round(h1,5)) + "\n")
+        file.write("P(h2 | Q) = " + str(round(h2,5)) + "\n")
+        file.write("P(h3 | Q) = " + str(round(h3,5)) + "\n")
+        file.write("P(h4 | Q) = " + str(round(h4,5)) + "\n")
+        file.write("P(h5 | Q) = " + str(round(h5,5)) + "\n")
+        file.write("\nProbaility that the next candy we pick will be C, given Q: " + str(round(pNextC,5)))
+        file.write("\nProbaility that the next candy we pick will be L, given Q: " + str(round(pNextL,5)))
 
 except:
     print("File error.")
